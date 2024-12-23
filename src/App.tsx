9a3af720c0c7f39d3d8fec97ec5a7766b24451d3
beomsuKim <ta09472@naver.com>
2024-12-24 01:56:25 +0900

    feat: CountContext 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router";
 import Layout from "@/components/layout";
 import APage from "./pages/a-page";
+import { CountContextProvider } from "./context/count-context";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<APage />} />
-      </Route>
-    </Routes>
+    <CountContextProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<APage />} />
+        </Route>
+      </Routes>
+    </CountContextProvider>
   );
 }
 
--- src/pages/a-page.tsx
+++ src/pages/a-page.tsx
@@ -1,5 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { CountContext } from "@/context/count-context";
+import { useContext } from "react";
 
 export default function APage() {
-  return <Button>A 페이지</Button>;
+  const { count, setCount } = useContext(CountContext);
+
+  return (
+    <Button onClick={() => setCount((prev) => prev + 1)} className="w-20">
+      {count}
+    </Button>
+  );
 }
