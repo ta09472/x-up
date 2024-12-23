3708d2f552e172487d39df6acac0af2b8f616011
beomsuKim <ta09472@naver.com>
2024-12-24 01:03:42 +0900

    feat: A페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css";
-import { Button } from "@/components/ui/button";
+import { Routes, Route } from "react-router";
+import Layout from "@/components/layout";
+import APage from "./pages/a-page";
 
 function App() {
   return (
-    <>
-      <Button>hello</Button>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<APage />} />
+      </Route>
+    </Routes>
   );
 }
 
