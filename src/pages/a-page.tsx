1961ef834b20041478a3267dbc924066be93dc32
beomsuKim <ta09472@naver.com>
2024-12-24 21:40:59 +0900

    chore: 설명을 위한 주석 추가

--- src/pages/a-page.tsx
+++ src/pages/a-page.tsx
@@ -6,18 +6,25 @@ export default function APage() {
   const { count, setCount } = useContext(CountContext);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prev) => {
-        if (prev >= 10) {
-          clearInterval(interval);
-          return prev;
+    console.log("✅ Creating an interval");
+    const id = setInterval(() => {
+      console.log("⏰ Interval tick");
+      setCount((prevCount) => {
+        if (prevCount === 10) {
+          console.log("❌ Clearing an interval by condition");
+          clearInterval(id);
+          return prevCount;
         }
-        return prev + 1;
+        return prevCount + 1;
       });
     }, 1000);
-    console.log(count);
 
-    return () => clearInterval(interval);
+    console.log(id);
+    return () => {
+      console.log("❌ Clearing an interval by unmounted");
+      clearInterval(id);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <Button className="w-20">{count}</Button>;
