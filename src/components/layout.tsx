b53b8a58a835e79ad76e62ed2fd7829435ac749a
beomsuKim <ta09472@naver.com>
2024-12-25 03:36:11 +0900

    feat: 레이아웃 및 버튼 기본 너비 조정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,49 +1,40 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-
 import { useNavigate } from "react-router";
 import { Outlet } from "react-router";
 import { Button } from "./ui/button";
 
+const items = [
+  {
+    label: "a",
+    path: "/",
+  },
+  {
+    label: "b",
+    path: "b",
+  },
+  {
+    label: "c",
+    path: "c",
+  },
+  {
+    label: "polygon",
+    path: "polygon",
+  },
+];
+
 export default function Layout() {
-  const items = [
-    {
-      label: "a",
-      path: "/",
-    },
-    {
-      label: "b",
-      path: "b",
-    },
-    {
-      label: "c",
-      path: "c",
-    },
-    {
-      label: "polygon",
-      path: "polygon",
-    },
-  ];
   const navigate = useNavigate();
 
   return (
-    <div className=" w-full h-svh flex flex-col items-center justify-center">
-      <div className=" border rounded-lg border-neutral-300 p-40">
-        <NavigationMenu>
-          <NavigationMenuList>
-            {items.map(({ label, path }) => (
-              <NavigationMenuItem key={label}>
-                <Button onClick={() => navigate(path)} variant="link">
-                  {label}
-                </Button>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
+    <div className=" w-full h-svh flex flex-col ">
+      <div className="h-14  border-b w-full sticky top-0 flex items-center  px-20 z-40 bg-white">
+        {items.map(({ label, path }) => (
+          <Button key={label} onClick={() => navigate(path)} variant="link">
+            {label}
+          </Button>
+        ))}
+      </div>
 
+      <div className=" min-w-[40rem] flex flex-col h-full justify-center items-center mx-auto px-20 z-10">
         <Outlet />
       </div>
     </div>
