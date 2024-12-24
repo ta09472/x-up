940afa59142123802abb017009a3475dfcaba858
beomsuKim <ta09472@naver.com>
2024-12-25 02:39:04 +0900

    fix: Provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import { Routes, Route } from "react-router";
 import Layout from "@/components/layout";
 import APage from "./pages/a-page";
-import { CountContextProvider } from "./context/count-context";
 import BPage from "./pages/b-page";
 import CPage from "./pages/c-page";
 import Polygon from "./pages/polygon";
 
 function App() {
   return (
-    <CountContextProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<APage />} />
-          <Route path="/b" element={<BPage />} />
-          <Route path="/c" element={<CPage />} />
-          <Route path="/polygon" element={<Polygon />} />
-        </Route>
-      </Routes>
-    </CountContextProvider>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<APage />} />
+        <Route path="/b" element={<BPage />} />
+        <Route path="/c" element={<CPage />} />
+        <Route path="/polygon" element={<Polygon />} />
+      </Route>
+    </Routes>
   );
 }
 
