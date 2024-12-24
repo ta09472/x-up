dd235f733cd2e32cb2a68c29131e15cbe9fbddbd
beomsu <55176563+ta09472@users.noreply.github.com>
2024-12-25 00:14:32 +0900

    Feature/xup 2 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import Layout from "@/components/layout";
 import APage from "./pages/a-page";
 import { CountContextProvider } from "./context/count-context";
+import BPage from "./pages/b-page";
+import CPage from "./pages/c-page";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<APage />} />
+          <Route path="/b" element={<BPage />} />
+          <Route path="/c" element={<CPage />} />
         </Route>
       </Routes>
     </CountContextProvider>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,47 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+import { useNavigate } from "react-router";
 import { Outlet } from "react-router";
+import { Button } from "./ui/button";
 
 export default function Layout() {
+  const items = [
+    {
+      label: "a",
+      path: "/",
+    },
+    {
+      label: "b",
+      path: "b",
+    },
+    {
+      label: "c",
+      path: "c",
+    },
+  ];
+  const navigate = useNavigate();
+
   return (
     <div className=" w-full h-svh flex flex-col items-center justify-center">
-      <Outlet />
+      <div className=" border rounded-lg border-neutral-300 p-40">
+        <NavigationMenu>
+          <NavigationMenuList>
+            {items.map(({ label, path }) => (
+              <NavigationMenuItem key={label}>
+                <Button onClick={() => navigate(path)} variant="link">
+                  {label}
+                </Button>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <Outlet />
+      </div>
     </div>
   );
 }
